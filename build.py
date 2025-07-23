3511152a788dda39a028a43bd47798a15401187d
Hearn <ayushkaintura1123@gmail.com>
2025-07-23 23:11:21 +0530

    worked

--- build.py
+++ build.py
@@ -1,32 +1,49 @@
 import os
 import subprocess
-
+import shutil
+import sys
 
 build_dir = "build"
-esp_path = os.path.join(build_dir, "esp/EFI/Boot")
-output_img = "boot.img"
+esp_dir = os.path.join(build_dir, "esp", "EFI", "Boot")
+output_img = os.path.join(build_dir, "boot.img")
+boot_efi = os.path.join(esp_dir, "BOOTX64.efi")
 
-os.makedirs(esp_path, exist_ok=True)
+os.makedirs(esp_dir, exist_ok=True)
 
-subprocess.run(["cmake", "-B", "build", "-DCMAKE_TOOLCHAIN_FILE=cmake/Toolchain.cmake"], check=True)
-subprocess.run(["cmake", "--build", "build"], check=True)
+subprocess.run([
+    "cmake",
+    "-B", build_dir,
+    "-DCMAKE_TOOLCHAIN_FILE=cmake/Toolchain.cmake"
+], check=True)
+
+subprocess.run([
+    "cmake", "--build", build_dir
+], check=True)
 
 if os.path.exists(output_img):
     os.remove(output_img)
 
-subprocess.run(["mkfs.vfat", "-C", output_img, "65536"], check=True)
-subprocess.run(["mcopy", "-i", output_img, "-s", "esp", "::/"], check=True)
+subprocess.run([
+    "mkfs.vfat", "-C", output_img, "65536"
+], check=True)
 
-# subprocess.run([
-#     "qemu-system-x86_64",
-#     "-bios", "/usr/share/ovmf/x64/OVMF.fd",
-#     "-drive", f"format=raw,file={output_img},if=virtio",
-#     "-serial", "mon:stdio"
-# ])
+subprocess.run([
+    "mcopy", "-i", output_img, "-s",
+    os.path.join(build_dir, "esp", "EFI"),
+    "::/EFI"
+], check=True)
+
+ovmf_code = "/usr/share/edk2/x64/OVMF_CODE.4m.fd"
+ovmf_vars = "OVMF_VARS.fd"  
+
+if not os.path.exists(ovmf_vars):
+    print(f"Missing {ovmf_vars}. Copy it from /usr/share/edk2/x64/OVMF_VARS.4m.fd")
+    sys.exit(1)
+
 subprocess.run([
     "qemu-system-x86_64",
-    "-drive", "if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd",
-    "-drive", "if=pflash,format=raw,file=OVMF_VARS.fd",  # you can make a writable copy of OVMF_VARS.4m.fd
+    "-drive", f"if=pflash,format=raw,readonly=on,file={ovmf_code}",
+    "-drive", f"if=pflash,format=raw,file={ovmf_vars}",
     "-drive", f"format=raw,file={output_img},if=virtio",
     "-serial", "mon:stdio"
 ])
