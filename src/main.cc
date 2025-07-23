6aa72f8ffcf8bcab97a53f988f0225db1a9f1319
Hearn <ayushkaintura1123@gmail.com>
2025-07-23 19:51:06 +0530

    build

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@ extern "C"
 EFI_STATUS
 efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE* system_table) {
     InitializeLib(image, system_table);
-    Print((CHAR16*)L"PrimusBoot: We live!\n");
+    Print((CHAR16*)L"Oblivion: We live!\n");
 
     EFI_INPUT_KEY key;
     system_table->ConIn->ReadKeyStroke(system_table->ConIn, &key);
